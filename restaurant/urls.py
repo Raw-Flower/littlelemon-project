ba5e2c4df21f86c6976ecb5a56ba3ee3eeff8fba
Daniel florez <florezx15@gmail.com>
2025-05-29 16:14:37 -0500

    API Config
    Configure API gateways

--- restaurant/urls.py
+++ restaurant/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from .views import index
+from .views import index, MenuItemView, SingleMenuItemView
 
 urlpatterns = [
-    path(route='',view=index,name='home')
+    path(route='',view=index,name='home'),
+    path(route='menu/', view=MenuItemView.as_view()),
+    path(route='menu/<int:pk>', view=SingleMenuItemView.as_view()),
 ]
--- restaurant/views.py
+++ restaurant/views.py
@@ -1,4 +1,8 @@
 from django.shortcuts import render
+from rest_framework.generics import ListCreateAPIView, RetrieveUpdateAPIView, DestroyAPIView
+from rest_framework.viewsets import ModelViewSet
+from .models import Booking, Menu
+from .serializers import BookingSerializer, MenuSerializer
 
 # Create your views here.
 def index(request):
@@ -9,3 +13,16 @@ def index(request):
             
         }
     )
+
+# API
+class MenuItemView(ListCreateAPIView):
+    queryset = Menu.objects.all()
+    serializer_class = MenuSerializer
+
+class SingleMenuItemView(RetrieveUpdateAPIView,DestroyAPIView):
+    queryset = Menu.objects.all()
+    serializer_class = MenuSerializer
+    
+class BookingViewSet(ModelViewSet):
+    queryset = Booking.objects.all()
+    serializer_class = BookingSerializer
